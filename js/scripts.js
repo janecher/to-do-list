d1b2ad4ad09fefdf33098e72c88ec9545611d96a
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-06-15 16:33:46 -0700

    add functions to show to-do-list
    Co-authored-by: Vanessa Guerrero <vamarilug@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,17 @@ ToDoList.prototype.addTask = function(task) {
   this.tasks.push(task);
 }
 
+ToDoList.prototype.findTask = function(taskId) {
+  for (let i=0; i < this.tasks.length; i++) {
+    if (this.tasks[i]) {
+      if (this.tasks[i].id === taskId) {
+        return this.tasks[i];
+      }
+    }
+  }
+  return false;
+}
+
 ToDoList.prototype.changeTask = function(taskId, newTask) {
   for (let i=0; i < this.tasks.length; i++) {
     if (this.tasks[i]) {
@@ -43,26 +54,47 @@ function Task (task){
   this.isDone = false;
 }
 
+Task.prototype.taskDone = function () {
+  this.isDone = true;
+}
+
 //UI Logic
 let toDoList = new ToDoList();
 
+function displayTasks(toDoListToDisplay){
+  let tasksList = $("ul#tasks");
+  let htmlForTasksInfo = "";
+  toDoListToDisplay.tasks.forEach(function(task) {
+    htmlForTasksInfo += "<li id=" + task.id + ">" + task.task + "</li>" + "<button class='deleteButton' id=" + task.id + ">Delete</button>";
+  });
+  tasksList.html(htmlForTasksInfo);  
+}
+
+function attachTaskListeners() {
+  $("ul#tasks").on("click", ".deleteButton", function() {
+    toDoList.deleteTask(this.id);
+    console.log(toDoList);
+    displayTasks(toDoList);
+  });
+};
+
 $(document).ready(function(){
+  attachTaskListeners();
   $("form").submit(function(event) {
     event.preventDefault();
-    $("ul#tasks").children().remove();
+    //$("ul#tasks").children().remove();
     let inputtedTask = $("input#task").val();
     if(!inputtedTask) {
       alert("Please input your task");
       return;
     } 
     let task = new Task(inputtedTask);
-    console.log(task);
     toDoList.addTask(task);
-    console.log(toDoList);
-    toDoList.tasks.forEach(function(element) {
+    $("input#task").val("");
+    /*toDoList.tasks.forEach(function(element) {
       $("ul#tasks").append("<li>" + element.task + "</li>");
     });
-    $(".to-dos").show();
-    $("input#task").val("");
+    $(".to-dos").show();*/
+    displayTasks(toDoList);
   });
 });
