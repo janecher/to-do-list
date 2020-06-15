c9ceb165cfdf57f6f72a62fd09342bd02d17c549
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-06-15 13:27:21 -0700

    add ToDoList and Task constructors, and main functions to manipulate with objects of that types
    Co-authored-by: Vanessa Guerrero <vamarilug@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,47 @@
+// Business Logic for ToDoList and Tasks
+function ToDoList (){
+  this.tasks = [];
+  this.currentId = 0;
+}
+
+ToDoList.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+ToDoList.prototype.addTask = function(task) {
+  task.id = this.assignId; 
+  this.tasks.push(task);
+}
+
+ToDoList.prototype.changeTask = function(taskId, newTask) {
+  for (let i=0; i < this.tasks.length; i++) {
+    if (this.tasks[i]) {
+      if (this.tasks[i].id === taskId) {
+        this.tasks[i].task = newTask;
+      }
+    }
+  }
+  return false;
+}
+
+ToDoList.prototype.deleteTask = function (taskId){
+  for (let i=0; i<this.tasks.length; i++) {
+    if (this.tasks[i]) {
+      if (this.tasks[i].id === taskId) {
+        delete this.tasks[i];
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function Task (task){
+  this.task = task;
+  this.isDone = false;
+}
+
 $(document).ready(function(){
 
 });
